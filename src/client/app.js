210488f31ee2b2f8116669a46e69da70f6b65c22
promise <hdqddwh@163.com>
2019-09-10 21:54:42 +0800

    update

--- src/client/app.js
+++ src/client/app.js
@@ -14,7 +14,7 @@ class App extends React.Component{
   render(){
     return (
       <div className="head">
-        <p>Learning React SSRRRRRRRRRR</p>
+        <p>Learning React SSR</p>
       </div>
     )
   }
